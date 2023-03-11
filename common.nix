3fdd738893f1cc9533e7575b9476904648f5c6b2
Sebastian Witte <woozletoff@gmail.com>
2023-03-11 19:16:37 +0000

    A mocha flavored starship

--- common.nix
+++ common.nix
@@ -203,10 +203,26 @@
       };
     };
 
-    starship = {
-      enable = true;
-      enableNushellIntegration = true;
-    };
+    starship =
+      let
+        flavour = "mocha"; # One of `latte`, `frappe`, `macchiato`, or `mocha`
+      in
+      {
+        enable = true;
+        enableNushellIntegration = true;
+        settings = {
+          # Other config here
+          format = "$all"; # Remove this line to disable the default prompt format
+          palette = "catppuccin_${flavour}";
+        } // builtins.fromTOML (builtins.readFile
+          (pkgs.fetchFromGitHub
+            {
+              owner = "catppuccin";
+              repo = "starship";
+              rev = "3e3e54410c3189053f4da7a7043261361a1ed1bc"; # Replace with the latest commit hash
+              sha256 = "soEBVlq3ULeiZFAdQYMRFuswIIhI9bclIU8WXjxd7oY=";
+            } + /palettes/${flavour}.toml));
+      };
     nushell = {
       enable = true;
       configFile.source = ./nushell/config.nu;
