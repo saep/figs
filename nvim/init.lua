14b6337a096ffa5864b21ef0e5be98b036fc82b8
Sebastian Witte <woozletoff@gmail.com>
2023-03-19 18:43:20 +0100

    Add rust-tools

--- nvim/init.lua
+++ nvim/init.lua
@@ -116,6 +116,28 @@ require("lazy").setup({
     "neovim/nvim-lspconfig",
   },
   {
+    "simrat39/rust-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    ft = { "rust", },
+    config = function()
+      local rt = require("rust-tools")
+      rt.setup({
+        server = {
+          on_attach = function(client, bufnr)
+            require("saep.lsp").on_attach(client, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      })
+    end,
+  },
+  {
     "glepnir/lspsaga.nvim",
     opts = {
       symbol_in_winbar = {
