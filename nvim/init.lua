a5a740c54118a87a4565e126296bc1e409183d25
Sebastian Witte <woozletoff@gmail.com>
2023-09-18 21:00:13 +0200

    Use flex layout strategy in telescope by default

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,7 +67,19 @@ require("lazy").setup({
     },
     config = function()
       require("telescope").load_extension("project")
-    end
+      require("telescope").setup({
+        defaults = {
+          layout_strategy = "flex",
+          layout_config = {
+            prompt_position = "bottom",
+            flip_columns = 160,
+            width = 0.99,
+            height = 0.99,
+          },
+        },
+
+      })
+    end,
   },
   {
     "folke/which-key.nvim",
