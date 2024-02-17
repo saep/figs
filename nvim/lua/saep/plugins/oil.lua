6a9608970ee9d908a37fe78e67cbb6451aa95467
Sebastian Witte <woozletoff@gmail.com>
2024-02-17 16:55:48 +0100

    Comment about downloading spell files for oil

--- nvim/lua/saep/plugins/oil.lua
+++ nvim/lua/saep/plugins/oil.lua
@@ -1,1 +1,4 @@
-require("oil").setup()
+require("oil").setup({
+  -- Temporarily set to false if you have netrw issues (e.g. when trying to download spell files)
+  -- default_file_explorer = false,
+})
