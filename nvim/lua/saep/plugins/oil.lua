aed6e808156dea4c03fdfd3d389c5767943e9134
Sebastian Witte <woozletoff@gmail.com>
2024-07-17 15:25:04 +0200

    Add detail shortcurt to oil which allows editing permissions

--- nvim/lua/saep/plugins/oil.lua
+++ nvim/lua/saep/plugins/oil.lua
@@ -1,4 +1,18 @@
+local detail = false
 require("oil").setup({
 	-- Temporarily set to false if you have netrw issues (e.g. when trying to download spell files)
 	-- default_file_explorer = false,
+	keymaps = {
+		["gd"] = {
+			desc = "Toggle file detail view",
+			callback = function()
+				detail = not detail
+				if detail then
+					require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+				else
+					require("oil").set_columns({ "icon" })
+				end
+			end,
+		},
+	},
 })
