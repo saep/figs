f8fb3d62bd1567e5adb9e5e591250bfc9efa3395
Sebastian Witte <saep@saep.rocks>
2024-11-03 21:43:17 +0100

    Add postfix snippets for paths and constants

--- nvim/snippets/all.lua
+++ nvim/snippets/all.lua
@@ -19,4 +19,14 @@ return {
 			return require("textcase").api.to_pascal_case(parent.snippet.env.POSTFIX_MATCH)
 		end, {}),
 	}),
+	postfix("/", {
+		f(function(_, parent)
+			return require("textcase").api.to_path_case(parent.snippet.env.POSTFIX_MATCH)
+		end, {}),
+	}),
+	postfix("!", {
+		f(function(_, parent)
+			return require("textcase").api.to_constant_case(parent.snippet.env.POSTFIX_MATCH)
+		end, {}),
+	}),
 }, {}
