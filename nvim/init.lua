8f0b76f6f6470e0f02ed00268a434300495aba43
Sebastian Witte <saep@saep.rocks>
2024-11-03 21:49:26 +0100

    Make nushell work in neovim as the default shell

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,15 @@
+-- nushell settings
+-- taken from: https://github.com/nushell/integrations/blob/main/nvim/init.lua
+vim.opt.sh = "nu"
+vim.opt.shelltemp = false
+vim.opt.shellredir = "out+err> %s"
+vim.opt.shellcmdflag = "--stdin --no-newline -c"
+vim.opt.shellxescape = ""
+vim.opt.shellxquote = ""
+vim.opt.shellquote = ""
+vim.opt.shellpipe =
+	"| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
+
 vim.loader.enable()
 
 P = function(t)
