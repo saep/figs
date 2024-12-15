1d9005af7619758920db06e56df019cac05ce6d0
Sebastian Witte <saep@saep.rocks>
2024-12-15 12:32:47 +0100

    Remove old code to figure out the highlighting group

--- nvim/after/plugin/init.lua
+++ nvim/after/plugin/init.lua
@@ -20,19 +20,6 @@ require("saep.keys")
 -- Must be set here for some reason as the hydras aren't colored otherwise
 vim.cmd.colorscheme("catppuccin")
 
--- Figure out highlighting group
-function SynStack()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  local linenr = cursor[1]
-  local columnnr = cursor[2] + 1
-  for _, synID in ipairs(vim.call("synstack", linenr, columnnr)) do
-    local i2 = vim.call("synIDtrans", synID)
-    local n1 = vim.call("synIDattr", synID, "name")
-    local n2 = vim.call("synIDattr", i2, "name")
-    vim.api.nvim_echo({ [1] = { n1 .. "->" .. n2 } }, nil, {})
-  end
-end
-
 -- Fix saving spell checking by prepending a specific writable directory to the runtimepath.
 vim.opt.rtp:prepend("~/.local/share/nvim/site/")
 
