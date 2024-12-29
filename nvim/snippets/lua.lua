cb5d59070347a3e9d33fd4ab214911c03d4076af
Sebastian Witte <saep@saep.rocks>
2024-12-29 15:29:24 +0100

    Remove leading , from lua snippets

--- nvim/snippets/lua.lua
+++ nvim/snippets/lua.lua
@@ -1,5 +1,5 @@
 return {
-  s(",d", {
+  s("d", {
     t('describe("'),
     i(1, "category"),
     t({ '", function()', '    it("' }),
@@ -8,14 +8,14 @@ return {
     i(0),
     t({ "", "    end)", "end)" }),
   }),
-  s(",it", {
+  s("it", {
     t('it("'),
     i(1, "test case"),
     t({ '", function()', "    " }),
     i(0),
     t({ "", "end)" }),
   }),
-  s(",r", {
+  s("r", {
     t('require("'),
     i(1),
     t('")'),
