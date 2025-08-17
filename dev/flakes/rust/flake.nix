d13644f2015ba8081e29e32c2487e81505d775eb
Sebastian Witte <saep@saep.rocks>
2025-08-17 22:34:02 +0200

    Simplify rust dev flake template

--- dev/flakes/rust/flake.nix
+++ dev/flakes/rust/flake.nix
@@ -1,67 +1,47 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-
-    # Dev tools
-    treefmt-nix.url = "github:numtide/treefmt-nix";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
-      perSystem = { config, self', pkgs, lib, system, ... }:
-        let
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          nonRustDeps = [
-            pkgs.libiconv
-            pkgs.openssl
-            pkgs.pkg-config
-          ];
-        in
-        {
-          # Rust package
-          packages.default = pkgs.rustPlatform.buildRustPackage {
-            inherit (cargoToml.package) name version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      naerskLib = pkgs.callPackage naersk { };
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          bacon
+          bunyan-rs
+          cargo
+          cargo-edit
+          cargo-nextest
+          clippy
+          comrak
+          rust-analyzer
+          rustc
+        ];
 
-          # Rust dev environment
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [
-              config.treefmt.build.devShell
-            ];
-            shellHook = ''
-              # For rust-analyzer 'hover' tooltips to work.
-              export RUST_SRC_PATH=${pkgs.rustPlatform.rustLibSrc}
-            '';
-            buildInputs = nonRustDeps;
-            nativeBuildInputs = with pkgs; [
-              bacon
-              just
-              rustc
-              cargo
-              cargo-nextest
-              cargo-watch
-              clippy
-              rust-analyzer
-            ];
-          };
-
-          # Add your auto-formatters here.
-          # cf. https://numtide.github.io/treefmt/
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixpkgs-fmt.enable = true;
-              rustfmt.enable = true;
-            };
-          };
-        };
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          libiconv
+          openssl
+        ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+      packages."x86_64-linux".default = naerskLib.buildPackage {
+        src = ./.;
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          libiconv
+          openssl
+        ];
+      };
     };
 }
