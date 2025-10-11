e0fefce585299cd19fd95d847f3f52bf0c3d93b4
Sebastian Witte <saep@saep.rocks>
2025-10-11 17:13:22 +0200

    Remove deprecated setup of nvim-ts-autotag

--- nvim/lua/saep/plugins/treesitter.lua
+++ nvim/lua/saep/plugins/treesitter.lua
@@ -8,9 +8,6 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  autotag = {
-    enable = true,
-  },
   incremental_selection = {
     enable = true,
     keymaps = {
