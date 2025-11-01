818aa5f74c5cc33990b2bebe2443575226202b9e
Sebastian Witte <saep@saep.rocks>
2025-11-01 19:37:45 +0100

    Add snippet to insert current date time

--- nvim/snippets/all.lua
+++ nvim/snippets/all.lua
@@ -29,4 +29,22 @@ return {
       return require("textcase").api.to_constant_case(parent.snippet.env.POSTFIX_MATCH)
     end, {}),
   }),
+  s({ trig = "now", name = "now as iso timestamp" }, {
+    c(1, {
+      f(function()
+        local res = vim.system({ "date", "--iso-8601=seconds" }, { text = true }):wait()
+        local stdout = res.stdout
+        local end_index = string.find(stdout, "\n") or stdout:len()
+        return '"' .. string.sub(stdout, 1, end_index - 1) .. '"'
+      end, {}),
+      f(function()
+        local res = vim.system({ "date", "--utc", "--iso-8601=seconds" }, { text = true }):wait()
+        local stdout = res.stdout
+        local end_index = string.find(stdout, "\n") or stdout:len()
+        return '"' .. string.sub(stdout, 1, end_index - 1) .. '"'
+      end, {}),
+      i(1),
+    }),
+    i(0),
+  }),
 }, {}
