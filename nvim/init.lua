3ac9888e6662d741dcf1594b15839b9a30bb5045
Sebastian Witte <saep@saep.rocks>
2026-03-15 10:56:13 +0100

    Set termguicolors because an error was shown

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,7 @@ vim.opt.shellxquote = ""
 vim.opt.shellquote = ""
 vim.opt.shellpipe =
   "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
+vim.opt.termguicolors = true
 
 vim.loader.enable()
 
